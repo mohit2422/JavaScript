3150ac0e216963d798ac1b6032bbaa8332736895
Mohit Kannaujiya <123012345+mohit2422@users.noreply.github.com>
2024-05-04 18:37:00 +0000

    conversion operation

--- 01_Basics/04_conversion_opeation.js
+++ 01_Basics/04_conversion_opeation.js
@@ -16,7 +16,7 @@ let valueInNumber = Number(score)
 /****************** String to Boolean ************************/
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /****************** Outputs for different values of isLoggedIn ************************/
 // 1 => true , 0 => false
@@ -26,5 +26,37 @@ console.log(booleanIsLoggedIn);
 /****************** Number to String ************************/
 let somenumber = 33
 let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/****************** Operations ************************/
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(4/3);
+// console.log(4%3);
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+// console.log(+"");
+let num1, num2, num3
+num1 = num2 = num3 = 2+3
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
