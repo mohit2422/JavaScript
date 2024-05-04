a4cb62693ea423b73260e41005d857bf7dcaf54b
Mohit Kannaujiya <123012345+mohit2422@users.noreply.github.com>
2024-05-04 19:20:17 +0000

    comparison of datatypes

---
+++ 01_Basics/05_comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// here it convert string datatype into number and then compare, 
+// for Hello on coverting its datatype became NaN
+// console.log("2" > 1);
+// console.log("02" > 1);
+// console.log("Hello" > 1);
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null==0);
+// here for equality comparison null datatype changed to NaN
+// for inequality comparison null datatype value changed to 0
+
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+// for undefined every comaparison give false result
+
+// sricty equal checks both data and datatypes comparison
+console.log("2"===2);
+
