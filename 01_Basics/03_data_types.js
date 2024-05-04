946d5dd19c9cb92e757992547306e38b0516e1f0
Mohit Kannaujiya <123012345+mohit2422@users.noreply.github.com>
2024-05-04 16:17:22 +0000

    datatypes

---
+++ 01_Basics/03_data_types.js
@@ -0,0 +1,21 @@
+"use strict"; // Treat all the JS code as newer version
+
+// alert(3+3)  // we are using nodejs not browser
+
+let name = "Mohit"
+let age = 22
+let isLoggedIn = false
+let state
+
+// number => 2^53
+// bigint
+// string = ""
+// boolean => true/false
+// undefined =>
+// symbol
+
+// object
+
+console.log(typeof undefined);   // undefined
+console.log(typeof null);  // object
+
